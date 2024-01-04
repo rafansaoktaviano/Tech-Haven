ff0f8f9a82225d428660c1c61de00223c91a6b9d
Rafansa Oktaviano <ravansaoktaviano@gmail.com>
2024-01-04 10:37:28 +0700

    fix cors

--- projects/server/src/lib/multer.js
+++ projects/server/src/lib/multer.js
@@ -38,38 +38,43 @@
 
 // module.exports = fileUploader;
 
-const multer = require('multer');
-const fs = require ('fs')
+const multer = require("multer");
+const fs = require("fs");
 // var public = require('.././public');
 
-const defaultPath = 'src/public' // ini diisi dengan folder tujuan dimana kita akan ngesave uploadan user
-const storage = multer.diskStorage({// Ini setup untuk storagenya, dimana letaknya kita akan menyimpan data
-    destination: async(req, file, cb) => {
-        const isDirectoryExist = fs.existsSync(defaultPath)
-
-        if(!isDirectoryExist){ //Kondisi jika directory belum dibuat atau gak ada, ini bakalan membuat directory baru
-            await fs.promises.mkdir(defaultPath, {recursive: true})
-        }
-        cb(null, `${defaultPath}`)
-    },
-    filename: function (req, file, cb) {
-        const extension = (file.mimetype.split("/")[1])
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + `.${extension}`
-        // console.log(uniqueSuffix)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
+const defaultPath = "src/public"; // ini diisi dengan folder tujuan dimana kita akan ngesave uploadan user
+const storage = multer.diskStorage({
+  // Ini setup untuk storagenya, dimana letaknya kita akan menyimpan data
+  destination: async (req, file, cb) => {
+    try {
+      if (!fs.existsSync(defaultPath)) {
+        await fs.promises.mkdir(defaultPath, { recursive: true });
+      }
+      cb(null, defaultPath);
+    } catch (error) {
+      console.error("Error creating directory:", error);
+      cb(error);
     }
-})
+  },
+  filename: function (req, file, cb) {
+    const extension = file.mimetype.split("/")[1];
+    const uniqueSuffix =
+      Date.now() + "-" + Math.round(Math.random() * 1e9) + `.${extension}`;
+    // console.log(uniqueSuffix)
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
 //Setup File Filter
 var fileFilter = (req, file, cb) => {
-    // console.log(file)
-    if(file.mimetype.split('/')[0]=== 'image'){
-        //Accept
-        cb(null, true)
-    } else if(file.mimetype.split('/')[0] !=='image'){
-        //Reject
-        cb(new Error('File must be image!'))
-    }
-}
+  // console.log(file)
+  if (file.mimetype.split("/")[0] === "image") {
+    //Accept
+    cb(null, true);
+  } else if (file.mimetype.split("/")[0] !== "image") {
+    //Reject
+    cb(new Error("File must be image!"));
+  }
+};
 
-exports.multerupload = multer({storage:storage, fileFilter:fileFilter})
+exports.multerupload = multer({ storage: storage, fileFilter: fileFilter });
